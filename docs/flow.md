bfb36e71499dfe29e9448d216da2e1d11df0a391
Paul Cleary <pauljohncleary@gmail.com>
2015-10-05 21:02:08 +0100

    ultra basic ft working

--- docs/flow.md
+++ docs/flow.md
@@ -1,15 +1,20 @@
 Sender:
 - Open page
-- Send file button / drag n drop
+- Send file button / drag n drop & shareable link shows up
 - Select file
-- Add password
-- Shareable link shows up
+- Add password (opt)
 - Send link to peer for download and password in a separate channel
+- Peer connects
+- SEND BUTTON PRESSED
+- Files finish transferring, room closed, peer disconnected
 
 Peer:
 - open Link
-- prompted for password
+- prompted for password (opt)
 - password is hashed and sent over webrtc and validated on the sender's machine
 - webrtc transaction begins and file is sent directly to the other user
 
-close browser, connection is dropped everything disappears
+
+When you land on the page you should be able to share the link, add the file, password etc.
+
+We have a separate area that's grey until the peer arrives
--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,59 @@
-//var $ = require('jquery');
+var $ = require('jquery');
 var simpleWebRTC = require('simplewebrtc');
+global.jQuery = global.$ = $;
 
+var webrtc = new simpleWebRTC({
+    // we don't do video
+    localVideoEl: '',
+    remoteVideosEl: '',
+    // dont ask for camera access
+    autoRequestMedia: false,
+    // dont negotiate media
+    receiveMedia: {
+        mandatory: {
+            offerToReceiveAudio: false,
+            offerToReceiveVideo: false
+        }
+    }
+});
 
-//global.jQuery = global.$ = $;
-
-alert("Hello Woorld!");
-
-//url will contain the remote peer id, e.g https://securefiles.com/sadf3r3
+//url will contain the remote peer id, e.g https://senditdirect.com/sadf3r3
 var remotePeerId = window.location.pathname.substr(1);
 console.log(remotePeerId);
+webrtc.joinRoom('sharingIdFromUrl');
 
-//after connecting to webrtc?
-//we connect to that peer directly automatically
+// called when a peer is created
+webrtc.on('createdPeer', function (peer) {
+    console.log('peer connected', peer);
 
-//display file send button once connection is established
+  // receiving an incoming filetransfer
+  peer.on('fileTransfer', function (metadata, receiver) {
+      console.log('incoming filetransfer', metadata.name, metadata);
+      receiver.on('progress', function (bytesReceived) {
+          console.log('receive progress', bytesReceived, 'out of', metadata.size);
+      });
+      // get notified when file is done
+      receiver.on('receivedFile', function (file, metadata) {
+          console.log('received file', metadata.name, metadata.size);
+
+          // close the channel
+          receiver.channel.close();
+      });
+      filelist.appendChild(item);
+  });
+
+  // send a file
+  fileinput.addEventListener('change', function() {
+      fileinput.disabled = true;
+
+      var file = fileinput.files[0];
+      var sender = peer.sendFile(file);
+  });
+
+
+});
+
+// select a file
+var fileinput = document.createElement('input');
+fileinput.type = 'file';
+document.getElementById('remotes').appendChild(fileinput);
